2d210d086256320d942132f9b88b5a43486113d7
pit_duw <pit.duw@gmail.com>
2019-12-09 02:26:47 +0100

    slightly less wrong than before

--- xsec.py
+++ xsec.py
@@ -1,6 +1,8 @@
 from __future__ import division
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import os
 import sys
 from scipy import integrate
@@ -10,36 +12,29 @@ import lhapdf
 # PDF indices: 1 d, 2 u, 21 g 
 
 JPsiMass = 3.096 #Gev
-params = [0.55, 0.283, 4.5, 2]    # lambda, kappa, <P_T>, n
+params = [0.284, 0.55, 4.5, 2]    # lambda, kappa, <P_T>, n
 
 ppdf = lhapdf.mkPDF("nCTEQ15_208_82", 0)
+#ppdf = lhapdf.mkPDF("nCTEQ15_208_82/0")
 
 #print((1+ np.heaviside(3.5**2 - params[2]**2) * params[1]/params[3] *(3.5**2 - params[2]**2)/(JPsiMass**2))**(-params[3]))
 
-def amplitude(x1, x2, P_T, s, params):
-    return params[0]**2*params[1]*s*x1*x2/(JPsiMass**2) * np.exp(-params[1]*min(P_T**2, params[2])/(JPsiMass**2)) * (1+ np.heaviside(P_T**2, params[2]**2) * params[1]/params[3] *(P_T**2 - params[2]**2)/(JPsiMass**2))**(-params[3])
+def amplitude(P_T, params): # Amplitude (without x dependence -> pulled into integrand)
+    return params[0]**2*params[1]/(JPsiMass**2) * np.exp(-params[1]*min(P_T**2, params[2]**2)/(JPsiMass**2)) * (1+ np.heaviside(P_T**2-params[2]**2, 0) * params[1]/params[3] *(P_T**2 - params[2]**2)/(JPsiMass**2))**(-params[3])
 
-def integrand(P_T, s, params):
-    return lambda x1, x2: ppdf.xfxQ2(21, x1, JPsiMass**2+P_T**2)*ppdf.xfxQ2(21, x2, JPsiMass**2+P_T**2)*amplitude(x1, x2, P_T, s, params)
+def integrand(P_T): # Integrand, including potential x dependence of the amplitude
+    return lambda x1: ppdf.xfxQ2(21, x1, JPsiMass**2+P_T**2)
 
-def xsec(P_T, s, params, pdf):
-    return 1/(2*s)*integrate.nquad(integrand(P_T, s, params), [[0,1],[0,1]])[0]
-
-
-#npdf = [lhapdf.mkPDF("nCTEQ15_208_82", ev) for ev in range(33)]
-print(389.4*xsec(3, 7000**2, params, ppdf))
-
-1/0
-
-x_vals = np.logspace(-4, 0, 100)
-pdf_vals = np.array([ppdf.xfxQ(1, x, 2)-ppdf.xfxQ(-1, x, 2) for x in x_vals])
-updf = lambda x: ppdf.xfxQ(2, x, 2)/x-ppdf.xfxQ(-2, x, 2)/x
+def intsquared(P_T, pdf):
+    integral = integrate.quad(integrand(P_T), 0,1, limit = 50000)
+    return integral[0]**2
 
 
-print(integrate.quad(updf,0.0000,1))
+PT_vals = np.linspace(0.5, 13.5, 140)
+xs_vals = np.array([0.5*389400*amplitude(pt, params)*intsquared(pt, ppdf) for pt in PT_vals])
+#print([(PT_vals[i], xs_vals[i]) for i in range(14)])
 
-#plt.plot(x_vals, pdf_vals)
-#plt.xscale("log")
-#plt.show()
-
-
+plt.plot(PT_vals, xs_vals)
+plt.yscale("log")
+plt.savefig("test.png")
+plt.show()
